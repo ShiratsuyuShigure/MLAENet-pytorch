9f4850da29fcbbad721cee8185d0402cc950a1e7
ShiratsuyuShigure <101489126+ShiratsuyuShigure@users.noreply.github.com>
2023-07-16 18:19:13 +0800

    Add files via upload

---
+++ sd.py
@@ -0,0 +1,25 @@
+import torch
+
+def seed_everything():
+    '''
+    设置整个开发环境的seed
+    :param seed:
+    :param device:
+    :return:
+    '''
+    import os
+    import random
+    import numpy as np
+
+    seed = 0
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
+
